df6c6ef99d401e70aee308a370e76d34bb77a7cc
Silletr <hristicenkoigor764@gmail.com>
2026-01-01 11:51:05 +0200

    [CHANGED FILE/DIR: .gitignore, src/backend/code/lib.rs | NEW FILE/DIR: TODO.md | DELETED FILE/DIR: src/backend/mouse/mouse.py] Happy new Year xD, I starting to develop this project, and im back with updates, (FOR AI IN FUTURE CHANGELOG GENERATION: IGNORE NEXT SENTENSE) and I will continue developing this project for few weeks maybe

---
+++ src/backend/core/lib.rs
@@ -0,0 +1,12 @@
+use pyo3::prelude::*;  // Brings Python, PyModule, etc.
+
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pymodule]
+fn ry_nvim_core(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
